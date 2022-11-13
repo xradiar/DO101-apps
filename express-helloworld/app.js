270d5e8c68a642f3bb3cebae1605292eba85a417
xradiar <xradiar@gmail.com>
2022-11-13 13:48:01 +0900

    add /mars endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello WdfADFadsforld!\n');
+});
+
+app.get('/mars', function(req, res) {
+  res.send('Hello Mars\n');
 });
 
 app.listen(8080, function () {
